a0dcb2445e06ca189e550123b216a471846dc2a3
krishna319 <km83843@gmail.com>
2021-08-05 09:42:19 +0530

    welcome to linecomparision

---
+++ src/line/line.java
@@ -0,0 +1,17 @@
+package line;
+
+public class line {
+	public static void main(String[] args) {
+		System.out.println("welcome to line comparision");
+		int x1 = 5;
+		int x2 = 7;
+		int y1 = 8;
+		int y2 = 89;
+
+		double lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1)
+				* (y2 - y1));
+
+		System.out.println(lengthOfLine1);
+
+	}
+}
