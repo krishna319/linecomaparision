1ce43415d7d99f7c46dfb2a54c75e493616bcb3d
krishna319 <km83843@gmail.com>
2021-08-05 09:52:38 +0530

    modified:line comaparision

--- src/line/line.java
+++ src/line/line.java
@@ -8,8 +8,7 @@ public class line {
 		int y1 = 8;
 		int y2 = 89;
 
-		double lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1)
-				* (y2 - y1));
+		double lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1)* (y2 - y1));
 
 		System.out.println(lengthOfLine1);
 
